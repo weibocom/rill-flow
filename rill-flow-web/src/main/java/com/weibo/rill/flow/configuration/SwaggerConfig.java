fd85a8ff3a2ea4b2195a5b99c6b59bae80e98792
刘泽宇 <zeyu10@staff.weibo.com>
2023-12-06 11:59:06 +0800

    remove useless import in SwaggerConfig

--- rill-flow-web/src/main/java/com/weibo/rill/flow/configuration/SwaggerConfig.java
+++ rill-flow-web/src/main/java/com/weibo/rill/flow/configuration/SwaggerConfig.java
@@ -8,7 +8,6 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 public class SwaggerConfig {
