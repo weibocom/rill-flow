a9bd57d777327b78f368dfb2d96e7b062b086d66
chenhao <chenhao16@staff.weibo.com>
2023-12-06 14:49:15 +0800

    fix setenv cannot be executed

--- docker/setenv.sh
+++ docker/setenv.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [[ ! -z "$RILL_FLOW_TRACE_ENDPOINT" ]]; then
+if [ ! -z "$RILL_FLOW_TRACE_ENDPOINT" ]; then
   export JAVA_TOOL_OPTIONS=-javaagent:/usr/local/tomcat/temp/opentelemetry-javaagent.jar
   export OTEL_EXPORTER_OTLP_ENDPOINT=$RILL_FLOW_TRACE_ENDPOINT
   export OTEL_SERVICE_NAME=rill-flow
