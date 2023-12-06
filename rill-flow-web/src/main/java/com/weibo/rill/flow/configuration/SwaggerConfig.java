24b40350675c0c875539b58da8cf2b6b4e123427
刘泽宇 <zeyu10@staff.weibo.com>
2023-12-06 11:29:19 +0800

    upgrade spring-kafka and swagger

--- rill-flow-web/src/main/java/com/weibo/rill/flow/configuration/SwaggerConfig.java
+++ rill-flow-web/src/main/java/com/weibo/rill/flow/configuration/SwaggerConfig.java
@@ -11,7 +11,6 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
     @Bean
     public Docket api() {
