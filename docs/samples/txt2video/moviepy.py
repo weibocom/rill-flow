18ff42aa7de069c653bc69b870f782a16d3c71ad
zhaozhenfei <353149695@qq.com>
2023-12-13 16:40:44 +0800

    Update moviepy.py

--- docs/samples/txt2video/moviepy.py
+++ docs/samples/txt2video/moviepy.py
@@ -1,17 +1,17 @@
 # main.py
+class Item(BaseModel):
+    segments: List[str]
+
+
 @app.post("/moviepy/generate")
-def concatenate_video_async(execution_id: str, name: str, request: dict):
-    executor.submit(concatenate_video, execution_id, name, request)
+def concatenate_video_async(execution_id: str, item: Item, request: Request):
+    executor.submit(concatenate_video, execution_id, item.segments, request.headers.get("X-Callback-Url"))
     return {"result": "success"}
 
 
-def concatenate_video(execution_id: str, name: str, request: dict):
-    clips = []
-    for segment in sorted(request["segments"]):
-        clip = VideoFileClip(segment)
-        clips.append(clip)
-
+def concatenate_video(execution_id: str, segments: List, callback_url: str):
     # merge video
+    clips = [VideoFileClip(segment) for segment in sorted(segments)]
     final_clip = concatenate_videoclips(clips)
     video_path = data_dir + "/" + execution_id
     if not os.path.exists(video_path):
@@ -24,27 +24,20 @@ def concatenate_video(execution_id: str, name: str, request: dict):
     generator = lambda txt: TextClip(txt, font='Arial', fontsize=52, color='white')
     subs = SubtitlesClip('./subtitles.srt', generator)
     subtitles = SubtitlesClip(subs, generator)
-    video_with_subtitles = CompositeVideoClip([video, subtitles.set_position(lambda t: ('center', 2000+t))])
+    video_with_subtitles = CompositeVideoClip([video, subtitles.set_position(lambda t: ('center', 2000 + t))])
     video_subtitles_path = video_path + "/subtitles_output.mp4"
     video_with_subtitles.write_videofile(video_subtitles_path)
 
-    finish_task(execution_id, name, video_subtitles_path)
-
-
-def finish_task(execution_id, name, video_path):
-    headers = {"Content-Type": "application/json"}
-    RILL_FLOW_HOST = os.environ.get(
-        "RILL_FLOW_HOST", "127.0.0.1"
-    )
-    RILL_FLOW_PORT = os.environ.get(
-        "RILL_FLOW_PORT", "8080"
-    )
-    url = f"http://{RILL_FLOW_HOST}:{RILL_FLOW_PORT}/flow/finish.json"
-    params = {'execution_id': execution_id, 'task_name': name}
-    data = {
+    callback_body = {
         "video_path": video_path
     }
-    requests.post(url, data=json.dumps(data), params=params, headers=headers)
+    callback(callback_url, callback_body)
+
+
+def callback(callback_url, callback_body):
+    headers = {"Content-Type": "application/json"}
+    payload = json.dumps(callback_body)
+    requests.post(callback_url, headers=headers, data=payload)
 
 
 if __name__ == '__main__':
