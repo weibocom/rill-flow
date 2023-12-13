c8bf1beaafa8bab3757ec7f7493f1d2209bcef4f
zhaozhenfei <353149695@qq.com>
2023-12-13 16:24:23 +0800

    Update bark.py

--- docs/samples/txt2video/bark.py
+++ docs/samples/txt2video/bark.py
@@ -1,9 +1,6 @@
 #main.py
-@app.post("/bark/generate")
-def bark_generator_async(execution_id: str, name: str, request: dict):
-    txt = request["txt"]
-    executor.submit(transformers_genarate, execution_id, name, txt)
-    return {"result": "success"}
+class Item(BaseModel):
+    text: str
 
 
 @app.on_event("startup")
@@ -11,43 +8,42 @@ async def on_startup():
     load_model()
 
 
+@app.post("/bark/generate")
+def bark_generator_async(execution_id: str, name: str, item: Item, request: Request):
+    executor.submit(transformers_generate, execution_id, name, item.text, request.headers.get("X-Callback-Url"))
+    return {"result": "success"}
+
+
 def load_model():
+    model_path = os.getcwd() + "/bark/models/bark"
     global bark_model, processor, voice_preset
     bark_model = BarkModel.from_pretrained(model_path)
     processor = AutoProcessor.from_pretrained("suno/bark")
     voice_preset = "v2/en_speaker_9"
 
 
-def transformers_genarate(excution_id: str, name: str, txt: str):
-    inputs = processor(txt, voice_preset=voice_preset)
+def transformers_generate(execution_id: str, name: str, text: str, callback_url: str):
+    inputs = processor(text, voice_preset=voice_preset)
     audio_array = bark_model.generate(**inputs)
     audio_array = audio_array.cpu().numpy().squeeze()
 
-    # save audio to disk
-    audio_path = data_dir + "/" + excution_id + "/wav"
-    if not os.path.exists(audio_path):
-        os.makedirs(audio_path)
-    audio_name = audio_path + "/" + name + ".wav"
-    write_wav(audio_name, SAMPLE_RATE, audio_array)
+    output_directory = data_dir + "/" + execution_id + "/wav"
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+    output_file_path = output_directory + "/" + name + ".wav"
+    write_wav(output_file_path, SAMPLE_RATE, audio_array)
 
-    # finish task callback
-    finish_task(excution_id, name, audio_name)
-
-
-def finish_task(execution_id, name, audio_path):
-    headers = {"Content-Type": "application/json"}
-    RILL_FLOW_HOST = os.environ.get(
-        "RILL_FLOW_HOST", "127.0.0.1"
-    )
-    RILL_FLOW_PORT = os.environ.get(
-        "RILL_FLOW_PORT", "8080"
-    )
-    url = f"http://{RILL_FLOW_HOST}:{RILL_FLOW_PORT}/flow/finish.json"
-    params = {'execution_id': execution_id, 'task_name': name}
-    data = {
-        "audio_path": audio_path
+    callback_body = {
+        "audio_path": output_file_path
     }
-    requests.post(url, data=json.dumps(data), params=params, headers=headers)
+    callback(callback_url, callback_body)
+
+
+def callback(callback_url, callback_body):
+    headers = {"Content-Type": "application/json"}
+    payload = json.dumps(callback_body)
+    response = requests.post(callback_url, headers=headers, data=payload)
+    print(response)
 
 
 if __name__ == '__main__':
