6ad00529cba630c7a44581655c53524aaeae859d
zhaozhenfei <353149695@qq.com>
2023-12-21 20:53:29 +0800

    Update moviepy

--- docs/samples/txt2video/moviepy/main.py
+++ docs/samples/txt2video/moviepy/main.py
@@ -1,51 +1,69 @@
 import json
 from concurrent.futures import ThreadPoolExecutor
 from typing import List
+
 import requests
 import uvicorn
 from fastapi import FastAPI, Request
 from moviepy.editor import *
-from moviepy.video.tools.subtitles import SubtitlesClip
 from pydantic import BaseModel
-
 
 app = FastAPI()
 executor = ThreadPoolExecutor(max_workers=20)
 data_dir = os.getenv('WORK_DIR', "/tmp")
 
 
-class Item(BaseModel):
+class Subtitles(BaseModel):
+    text: str
+    video_path: str
+
+
+class Concatenate(BaseModel):
     segments: List[str]
 
 
-@app.post("/moviepy/generate")
-def concatenate_video_async(execution_id: str, item: Item, request: Request):
-    executor.submit(concatenate_video, execution_id, item.segments, request.headers.get("X-Callback-Url"))
+@app.post("/moviepy/subtitles")
+def subtitles_async(execution_id: str, name: str, item: Subtitles, request: Request):
+    executor.submit(subtitles, execution_id, name, item.video_path, item.text, request.headers.get("X-Callback-Url"))
     return {"result": "success"}
 
 
-def concatenate_video(execution_id: str, segments: List, callback_url: str):
-    # merge video
-    clips = [VideoFileClip(segment) for segment in sorted(segments)]
-    final_clip = concatenate_videoclips(clips)
-    video_path = data_dir + "/" + execution_id
-    if not os.path.exists(video_path):
-        os.makedirs(video_path)
-    video_clip_path = video_path + "/merge_video_output.mp4"
-    final_clip.write_videofile(video_clip_path)
+@app.post("/moviepy/concatenate")
+def concatenate_async(execution_id: str, item: Concatenate, request: Request):
+    executor.submit(concatenate, execution_id, item.segments, request.headers.get("X-Callback-Url"))
+    return {"result": "success"}
 
-    # add subtitles
-    print(os.getcwd())
-    video = VideoFileClip(video_clip_path)
-    generator = lambda txt: TextClip(txt, font='Arial', fontsize=25, color='white')
-    subs = SubtitlesClip('./subtitles.srt', generator)
-    subtitles = SubtitlesClip(subs, generator)
-    video_with_subtitles = CompositeVideoClip([video, subtitles.set_position(lambda t: ('center', 1200 + t))])
-    video_subtitles_path = video_path + "/subtitles_output.mp4"
+
+def subtitles(execution_id: str, name: str, video_path: str, text: str, callback_url: str):
+    video_clip = VideoFileClip(video_path)
+    subtitle_clip = TextClip(text, font='Arial', fontsize=25, color='white', method='caption', size=video_clip.size, align='South')
+    subtitle_clip = subtitle_clip.set_duration(video_clip.duration)
+    video_with_subtitles = CompositeVideoClip([video_clip, subtitle_clip])
+
+    subtitles_video_path = data_dir + "/" + execution_id + "/subtitles"
+    if not os.path.exists(subtitles_video_path):
+        os.makedirs(subtitles_video_path)
+    video_subtitles_path = os.path.join(subtitles_video_path, f"{name}.mp4")
     video_with_subtitles.write_videofile(video_subtitles_path)
 
     callback_body = {
-        "video_path": video_path
+        "segment": video_subtitles_path
+    }
+    callback(callback_url, callback_body)
+
+
+def concatenate(execution_id: str, segments: List, callback_url: str):
+    clips = [VideoFileClip(segment) for segment in sorted(segments)]
+    final_clip = concatenate_videoclips(clips)
+
+    video_path = data_dir + "/" + execution_id
+    if not os.path.exists(video_path):
+        os.makedirs(video_path)
+    video_clip_path = video_path + "/result.mp4"
+    final_clip.write_videofile(video_clip_path)
+
+    callback_body = {
+        "result_video_path": video_clip_path
     }
     callback(callback_url, callback_body)
 
