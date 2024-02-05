cb4d9209342af048e92586009b1776861d7f3a90
zeyu10 <zeyu10@staff.sinal.com>
2024-02-05 15:57:39 +0800

    add enable field in columns

--- rill-flow-dag/olympicene-storage/src/main/java/com/weibo/rill/flow/olympicene/storage/dao/provider/TaskTemplateProvider.java
+++ rill-flow-dag/olympicene-storage/src/main/java/com/weibo/rill/flow/olympicene/storage/dao/provider/TaskTemplateProvider.java
@@ -28,7 +28,7 @@ import java.util.List;
 public class TaskTemplateProvider {
     private static final String TABLE_NAME = "task_template";
     private static final List<String> COLUMNS = Arrays.asList("id", "name", "type", "category", "icon", "task_yaml",
-            "schema", "output", "create_time", "update_time");
+            "schema", "output", "enable", "create_time", "update_time");
 
     @ResultType(Integer.class)
     public String insert(TaskTemplateDO taskTemplateDO) {
