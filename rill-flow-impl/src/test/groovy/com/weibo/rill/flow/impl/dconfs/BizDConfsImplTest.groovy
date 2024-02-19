f058b054f589b0648f7f762ec208600b19ac9837
zeyu10 <zeyu10@staff.sinal.com>
2024-02-19 17:52:09 +0800

    modify unit test to increase coverage

--- rill-flow-impl/src/test/groovy/com/weibo/rill/flow/impl/dconfs/BizDConfsImplTest.groovy
+++ rill-flow-impl/src/test/groovy/com/weibo/rill/flow/impl/dconfs/BizDConfsImplTest.groovy
@@ -30,4 +30,54 @@ class BizDConfsImplTest extends Specification {
         expect:
         bizDConfsImpl.getRedisBusinessIdToRuntimeCallbackContextMaxSize() == ['testBusiness01':3188, 'testBusiness02':9216]
     }
+
+    def "test redisBusinessIdToClientId"() {
+        expect:
+        bizDConfsImpl.getRedisBusinessIdToClientId() == ['testBusiness02':'testRedisClient']
+    }
+
+    def "test redisServiceIdToClientId"() {
+        expect:
+        bizDConfsImpl.getRedisServiceIdToClientId() == ['testBusiness01:testFeature01':'testRedisClient']
+    }
+
+    def "test redisBusinessIdToFinishReserveSecond"() {
+        expect:
+        bizDConfsImpl.getRedisBusinessIdToFinishReserveSecond() == ['testBusiness01':3600, 'testBusiness02':7200]
+    }
+
+    def "test redisBusinessIdToUnfinishedReserveSecond"() {
+        expect:
+        bizDConfsImpl.getRedisBusinessIdToUnfinishedReserveSecond() == ['testBusiness01':3600, 'testBusiness02':7200]
+    }
+
+    def "test redisBusinessIdToContextMaxLength"() {
+        expect:
+        bizDConfsImpl.getRedisBusinessIdToContextMaxLength() == ['testBusiness01':1024, 'testBusiness02':2048]
+    }
+
+    def "test redisBusinessIdToDAGInfoMaxLength"() {
+        expect:
+        bizDConfsImpl.getRedisBusinessIdToDAGInfoMaxLength() == ['testBusiness01':1024, 'testBusiness02':2048]
+    }
+
+    def "test swapBusinessIdToClientId"() {
+        expect:
+        bizDConfsImpl.getSwapBusinessIdToClientId() == ['testBusiness01':'testRedisClient']
+    }
+
+    def "test swapBusinessIdToFinishReserveSecond"() {
+        expect:
+        bizDConfsImpl.getSwapBusinessIdToFinishReserveSecond() == ['testBusiness01':3600]
+    }
+
+    def "test swapBusinessIdToUnfinishedReserveSecond"() {
+        expect:
+        bizDConfsImpl.getSwapBusinessIdToUnfinishedReserveSecond() == ['testBusiness01':3600]
+    }
+
+    def "test runtimeRedisStorageIdToMaxUsage"() {
+        expect:
+        bizDConfsImpl.getRuntimeRedisStorageIdToMaxUsage() == ['testRedisClient':85]
+    }
 }
