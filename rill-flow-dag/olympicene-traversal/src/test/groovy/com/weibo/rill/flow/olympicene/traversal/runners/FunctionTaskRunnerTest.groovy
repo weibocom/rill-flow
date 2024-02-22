436f556e4b061fddfc15a5188e7eadf97ccbd012
zeyu10 <zeyu10@staff.sinal.com>
2024-02-22 16:13:50 +0800

    add unit test to test base properties getter of FunctionTaskRunner

---
+++ rill-flow-dag/olympicene-traversal/src/test/groovy/com/weibo/rill/flow/olympicene/traversal/runners/FunctionTaskRunnerTest.groovy
@@ -0,0 +1,24 @@
+package com.weibo.rill.flow.olympicene.traversal.runners
+
+import com.alibaba.fastjson.JSONObject
+import spock.lang.Specification
+
+class FunctionTaskRunnerTest extends Specification {
+    FunctionTaskRunner runner = new FunctionTaskRunner(null, null, null, null, null, null)
+
+    def "test base properties getter"() {
+        expect:
+        runner.getCategory() == "function"
+        runner.getIcon() == ""
+    }
+
+    def "test getFields"() {
+        when:
+        JSONObject fields = runner.getFields()
+        then:
+        fields.size() == 12
+        fields.getJSONObject("next").getString("name") == "下一节点"
+        fields.getJSONObject("next").getString("type") == "string"
+        fields.getJSONObject("next").getBoolean("required") == false
+    }
+}
