d3dfc60769c91c1592b07e1b22e44526f51f6183
zeyu10 <zeyu10@staff.sina.com>
2024-06-25 11:56:53 +0800

    add unit test for DAGSubmitChecker

---
+++ rill-flow-service/src/test/groovy/com/weibo/rill/flow/service/statistic/DAGSubmitCheckerTest.groovy
@@ -0,0 +1,39 @@
+package com.weibo.rill.flow.service.statistic
+
+import com.weibo.rill.flow.olympicene.core.switcher.SwitcherManager
+import com.weibo.rill.flow.olympicene.storage.exception.StorageException
+import com.weibo.rill.flow.service.dconfs.BizDConfs
+import spock.lang.Specification
+
+class DAGSubmitCheckerTest extends Specification {
+    SwitcherManager switcherManager = Mock(SwitcherManager)
+    BizDConfs bizDConfs = Mock(BizDConfs)
+    DAGSubmitChecker dagSubmitChecker = new DAGSubmitChecker(switcherManagerImpl: switcherManager, bizDConfs: bizDConfs)
+
+    def setup() {
+        bizDConfs.getRedisBusinessIdToDAGInfoMaxLength() >> ["testBusiness1": 5]
+    }
+
+    def "test checkDAGInfoLength when switcher is off"() {
+        given:
+        switcherManager.getSwitcherState("ENABLE_DAG_INFO_LENGTH_CHECK") >> false
+        expect:
+        dagSubmitChecker.checkDAGInfoLength("testBusiness1:testFeatureName1_c_0dc48c1d-32a2", ["hello world".bytes])
+    }
+
+    def "test checkDAGInfoLength when switcher is on and be limited"() {
+        given:
+        switcherManager.getSwitcherState("ENABLE_DAG_INFO_LENGTH_CHECK") >> true
+        when:
+        dagSubmitChecker.checkDAGInfoLength("testBusiness1:testFeatureName1_c_0dc48c1d-32a2", ["hello world".bytes])
+        then:
+        thrown StorageException
+    }
+
+    def "test checkDAGInfoLength when switcher is on"() {
+        given:
+        switcherManager.getSwitcherState("ENABLE_DAG_INFO_LENGTH_CHECK") >> true
+        expect:
+        dagSubmitChecker.checkDAGInfoLength("testBusiness2:testFeatureName1_c_0dc48c1d-32a2", ["hello world".bytes])
+    }
+}
