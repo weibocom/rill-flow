659c60a59daec484c49f16c94c3c4844490c4d0e
zeyu10 <zeyu10@staff.sina.com>
2024-08-29 16:04:07 +0800

    get rootName out of loop

--- rill-flow-dag/olympicene-traversal/src/main/java/com/weibo/rill/flow/olympicene/traversal/runners/SwitchTaskRunner.java
+++ rill-flow-dag/olympicene-traversal/src/main/java/com/weibo/rill/flow/olympicene/traversal/runners/SwitchTaskRunner.java
@@ -134,8 +134,9 @@ public class SwitchTaskRunner extends AbstractTaskRunner {
                                               Set<String> skipTaskNames, Set<String> runTaskNames, DefaultSwitch defaultSwitch) {
         DAGWalkHelper dagWalkHelper = DAGWalkHelper.getInstance();
         boolean isAncestorTask = dagWalkHelper.isAncestorTask(taskInfo.getName());
+        String rootName = isAncestorTask? null: dagWalkHelper.getRootName(taskInfo.getName());
         Set<String> nextTaskNames = Arrays.stream(switchObj.getNext().split(",")).map(String::trim).filter(StringUtils::isNotBlank)
-                .map(it -> isAncestorTask ? it: dagWalkHelper.buildTaskInfoName(dagWalkHelper.getRootName(taskInfo.getName()), it))
+                .map(it -> isAncestorTask ? it: dagWalkHelper.buildTaskInfoName(rootName, it))
                 .collect(Collectors.toSet());
         boolean condition = judgeCondition(taskInfo, input, switchObj, defaultSwitch);
 
