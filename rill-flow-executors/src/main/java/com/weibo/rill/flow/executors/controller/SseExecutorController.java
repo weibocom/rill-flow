5d245b48236ffb8a428ddcae02455297136a6a95
zeyu10 <zeyu10@staff.sina.com>
2024-09-02 11:17:18 +0800

    add rest template

--- rill-flow-executors/src/main/java/com/weibo/rill/flow/executors/controller/SseExecutorController.java
+++ rill-flow-executors/src/main/java/com/weibo/rill/flow/executors/controller/SseExecutorController.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
 import java.util.HashMap;
@@ -17,13 +18,17 @@ import java.util.concurrent.ThreadPoolExecutor;
 public class SseExecutorController {
     @Resource
     private ThreadPoolExecutor sseThreadPoolExecutor;
+    @Resource
+    private RestTemplate restTemplate;
 
     @ApiOperation(value = "派发sse任务")
     @PostMapping(value = "dispatch.json")
     public Map<String, Object> dispatch(@ApiParam(value = "工作流执行ID") @RequestParam(value = "execution_id") String executionId,
                                         @ApiParam(value = "工作流执行ID") @RequestParam(value = "task_name") String taskName,
                                         @ApiParam(value = "任务详细信息") @RequestBody(required = false) SseDispatchParams params) {
-        sseThreadPoolExecutor.submit(() -> {});
+        sseThreadPoolExecutor.submit(() -> {
+
+        });
         return new HashMap<>();
     }
 }
