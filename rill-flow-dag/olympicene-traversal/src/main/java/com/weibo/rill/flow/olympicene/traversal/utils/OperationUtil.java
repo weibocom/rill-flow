ae70eaa9da8a08f1663788f4f1cb9bf046cebd3d
zeyu10 <zeyu10@staff.sina.com>
2024-10-14 16:45:40 +0800

    fix sonar: Refactor this code to use the more specialised Functional Interface 'ObjIntConsumer<Runnable>'

--- rill-flow-dag/olympicene-traversal/src/main/java/com/weibo/rill/flow/olympicene/traversal/utils/OperationUtil.java
+++ rill-flow-dag/olympicene-traversal/src/main/java/com/weibo/rill/flow/olympicene/traversal/utils/OperationUtil.java
@@ -2,15 +2,14 @@ package com.weibo.rill.flow.olympicene.traversal.utils;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.function.BiConsumer;
+import java.util.function.ObjIntConsumer;
 
 @Slf4j
 public class OperationUtil {
     private OperationUtil() {}
 
-    public static final BiConsumer<Runnable, Integer> OPERATE_WITH_RETRY = (operation, retryTimes) -> {
-        int exceptionCatchTimes = retryTimes;
-        for (int i = 1; i <= exceptionCatchTimes; i++) {
+    public static final ObjIntConsumer<Runnable> OPERATE_WITH_RETRY = (operation, retryTimes) -> {
+        for (int i = 1; i <= retryTimes; i++) {
             try {
                 operation.run();
                 return;
