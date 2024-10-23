ad0be5f20c2d8215e61386a2aabd632091a26da8
zeyu10 <zeyu10@staff.sina.com>
2024-10-23 11:37:12 +0800

    optimit code

--- rill-flow-service/src/main/java/com/weibo/rill/flow/service/strategies/ClientDAGProcessStrategy.java
+++ rill-flow-service/src/main/java/com/weibo/rill/flow/service/strategies/ClientDAGProcessStrategy.java
@@ -25,6 +25,7 @@ public class ClientDAGProcessStrategy implements DAGProcessStrategy {
 
     private static final String CONTEXT_PREFIX = "$.context.";
     private static final String INPUT_PREFIX = "$.input.";
+    private static final String DAG_END_TASK_NAME = "endPassTask";
 
     @Resource
     private DAGStringParser dagParser;
@@ -35,18 +36,17 @@ public class ClientDAGProcessStrategy implements DAGProcessStrategy {
      */
     @Override
     public DAG onStorage(DAG dag) {
-        // 1. 获取任务名称与任务的映射
         Map<String, BaseTask> taskMap = getTaskMapByDag(dag);
-        // 2. 处理 task 的 input 以及 dag 的 output，为任务生成原始的 inputMappings（input 中的来源直接作为 source，如 $.functionA.data.id）
+        // 1. 处理 task 的 input 以及 dag 的 output，为任务生成原始的 inputMappings（input 中的来源直接作为 source，如 $.functionA.data.id）
         // 返回是否需要后续处理，不需要后续处理则直接返回
         if (!processInputToGenerateInputMappings(dag, taskMap)) {
             return dag;
         }
-        // 3. 处理任务的 inputMappings，返回各任务 inputMappings 的 source 对应的元素列表的列表
+        // 2. 处理任务的 inputMappings，返回各任务 inputMappings 的 source 对应的元素列表的列表
         Map<String, List<List<String>>> taskPathsMap = processTaskInputMappings(dag, taskMap);
-        // 4. 通过各个任务 inputMappings 对应的元素列表的列表，生成任务的 outputMappings
+        // 3. 通过各个任务 inputMappings 对应的元素列表的列表，生成任务的 outputMappings
         LinkedHashMultimap<String, String> outputMappingsMultimap = getOutputMappingsByPaths(taskPathsMap);
-        // 5. 生成任务的输出映射
+        // 4. 将生成的 outputMappings 设置到对应的 task
         generateOutputMappingsIntoTasks(outputMappingsMultimap, taskMap);
         return dag;
     }
@@ -56,19 +56,17 @@ public class ClientDAGProcessStrategy implements DAGProcessStrategy {
      * @param dag DAG对象
      * @param taskMap 任务映射
      * @return 任务路径映射，如 functionB 任务的 inputMappings 包含两条，source 分别为：
-     * $["functionA"]["data"][0]["id"] 和 $["functionA"]["data"][0]["name"]
-     * 则返回： ["functionB": [["data", "0", "id"], ["data", "1", "id"]]]
+     *         $["functionA"]["data"][0]["id"] 和 $["functionA"]["data"][0]["name"]
+     *         则返回： ["functionB": [["data", "0", "id"], ["data", "1", "id"]]]
      */
     private Map<String, List<List<String>>> processTaskInputMappings(DAG dag, Map<String, BaseTask> taskMap) {
         Map<String, List<List<String>>> taskPathsMap = new HashMap<>();
-        for (BaseTask task : dag.getTasks()) {
-            processTaskInputMapping(task, taskMap, taskPathsMap, dag.getEndTaskName());
-        }
+        dag.getTasks().forEach(task -> processTaskInputMapping(task, taskMap, taskPathsMap, dag.getEndTaskName()));
         return taskPathsMap;
     }
 
     /**
-     * 分析和处理任务的 inputMappings
+     * 分析和处理任务的 inputMappings，并将结果填充到 taskPathsMap
      * @param task 待处理的任务
      * @param taskMap 任务映射
      * @param taskPathsMap 任务路径映射
@@ -109,8 +107,6 @@ public class ClientDAGProcessStrategy implements DAGProcessStrategy {
 
     /**
      * 将 taskName 放入到 task 的 next 中
-     * @param task 任务
-     * @param taskName 任务名称
      */
     private void updateTaskNext(BaseTask task, String taskName) {
         String next = task.getNext();
@@ -124,9 +120,7 @@ public class ClientDAGProcessStrategy implements DAGProcessStrategy {
     }
 
     /**
-     * 根据DAG获取任务映射
-     * @param dag DAG对象
-     * @return 任务名称到任务对象的映射
+     * 根据DAG获取任务名称到任务的映射
      */
     private Map<String, BaseTask> getTaskMapByDag(DAG dag) {
         if (CollectionUtils.isEmpty(dag.getTasks())) {
@@ -137,8 +131,6 @@ public class ClientDAGProcessStrategy implements DAGProcessStrategy {
 
     /**
      * 将 inputMapping 中的 source 解析为 element 数组，如 $["functionA"]["data"]["ids"], 则返回 ["functionA", "data", "ids"]
-     * @param inputMapping 输入映射
-     * @return jsonpath 元素数组
      */
     private List<String> getSourcePathElementsByMapping(Mapping inputMapping) {
         if (inputMapping.getSource() == null || !inputMapping.getSource().startsWith("$.")) {
@@ -159,9 +151,7 @@ public class ClientDAGProcessStrategy implements DAGProcessStrategy {
     }
 
     /**
-     * 将输出映射生成到任务中
-     * @param outputMappingsMultimap 输出映射多重映射
-     * @param taskMap 任务映射
+     * 将 outputMappings 设置到对应的任务中
      */
     private void generateOutputMappingsIntoTasks(LinkedHashMultimap<String, String> outputMappingsMultimap, Map<String, BaseTask> taskMap) {
         if (outputMappingsMultimap.isEmpty()) {
@@ -189,8 +179,6 @@ public class ClientDAGProcessStrategy implements DAGProcessStrategy {
 
     /**
      * 根据 path 元素列表，生成任务的 outputMappings
-     * @param taskPathsMap 任务路径映射
-     * @return 输出任务对应的 outputMappings
      */
     private LinkedHashMultimap<String, String> getOutputMappingsByPaths(Map<String, List<List<String>>> taskPathsMap) {
         LinkedHashMultimap<String, String> result = LinkedHashMultimap.create();
@@ -206,10 +194,9 @@ public class ClientDAGProcessStrategy implements DAGProcessStrategy {
     }
 
     /**
-     * 处理 jsonpath 对应的元素
-     * @param elements 路径元素列表
-     * @param result 结果多重映射
-     * @param taskName 任务名称
+     * 处理 jsonpath 对应的元素，并将结果设置到 result 中
+     * 如 elements 为 ["$", "context", "functionA", "data", "text"]
+     * 则生成的结果为：$.context.functionA.data.text
      */
     private void processPathElements(List<String> elements, LinkedHashMultimap<String, String> result, String taskName) {
         StringBuilder mappingSb = new StringBuilder();
@@ -228,10 +215,8 @@ public class ClientDAGProcessStrategy implements DAGProcessStrategy {
     }
 
     /**
-     * 处理 task 的 input 以及 dag 的 output，生成任务的 inputMappings
-     * @param dag DAG对象
-     * @param taskMap 任务映射
-     * @return 是否通过新版本的任务 input 或 DAG output 配置
+     * 根据 task 的 input 以及 dag 的 output，生成任务的 inputMappings
+     * @return 是否通过新版本的任务 input 或 DAG output 配置，意即是否需要后续处理
      */
     private boolean processInputToGenerateInputMappings(DAG dag, Map<String, BaseTask> taskMap) {
         if (CollectionUtils.isEmpty(dag.getTasks())) {
@@ -241,7 +226,7 @@ public class ClientDAGProcessStrategy implements DAGProcessStrategy {
         PassTask endPassTask = generateEndPassTask(dag, taskMap);
         boolean needPostProcess = endPassTask != null;
         // 2. 生成各个任务原始的 inputMappings，此时 source 内容仍然为 input 中的原始配置
-        // 如 input 为 id: $.functionA.id 则生成的 inputMapping 的source 为 $.functionA.id
+        // 如 input 为 "id: $.functionA.id" 则生成的 inputMapping 的 source 为 $.functionA.id，target 为 $.input.id
         for (BaseTask task : dag.getTasks()) {
             Map<String, Object> taskInput = task.getInput();
             if (MapUtils.isEmpty(taskInput)) {
@@ -250,56 +235,49 @@ public class ClientDAGProcessStrategy implements DAGProcessStrategy {
                 needPostProcess = true;
             }
             List<Mapping> inputMappings = task.getInputMappings() == null ? Lists.newArrayList() : task.getInputMappings();
-            for (Map.Entry<String, Object> entry : taskInput.entrySet()) {
-                String target = INPUT_PREFIX + entry.getKey();
+            taskInput.forEach((key, value) -> {
+                String target = INPUT_PREFIX + key;
+                if (key == null || value == null) {
+                    return;
+                }
                 Mapping inputMapping;
-                if (entry.getKey() == null || entry.getValue() == null) {
-                    continue;
-                } else if (entry.getValue() instanceof Map) {
-                    inputMapping = JSON.parseObject(JSON.toJSONString(entry.getValue()), Mapping.class);
+                if (value instanceof Map) {
+                    inputMapping = JSON.parseObject(JSON.toJSONString(value), Mapping.class);
                     inputMapping.setTarget(target);
                 } else {
-                    inputMapping = new Mapping(entry.getValue().toString(), target);
+                    inputMapping = new Mapping(value.toString(), target);
                 }
                 inputMappings.add(inputMapping);
-            }
+            });
             task.setInputMappings(inputMappings);
         }
         return needPostProcess;
     }
 
     /**
-     * 生成 end 任务，放置图的最终输出信息
-     * @param dag DAG对象
-     * @param taskMap 图全部任务 map
-     * @return 生成的 end 任务
+     * 生成 end 任务，用于设置图的最终输出信息
      */
     private PassTask generateEndPassTask(DAG dag, Map<String, BaseTask> taskMap) {
         if (MapUtils.isEmpty(dag.getOutput())) {
             return null;
         }
-        String endTaskName = "endPassTask";
-        dag.setEndTaskName(endTaskName);
+        dag.setEndTaskName(DAG_END_TASK_NAME);
         PassTask endPassTask = new PassTask();
-        endPassTask.setName(endTaskName);
+        endPassTask.setName(DAG_END_TASK_NAME);
         endPassTask.setInput(dag.getOutput());
         endPassTask.setCategory("pass");
         // 由于图的 end 任务没有后续任务，所以需要生成它的 outputMappings 来实现将参数传递的信息放入到 context 中
         List<Mapping> outputMappings = Lists.newArrayList();
-        for (String key : dag.getOutput().keySet()) {
-            outputMappings.add(new Mapping(INPUT_PREFIX + key, CONTEXT_PREFIX + key));
-        }
+        dag.getOutput().keySet().forEach(key -> outputMappings.add(new Mapping(INPUT_PREFIX + key, CONTEXT_PREFIX + key)));
         endPassTask.setOutputMappings(outputMappings);
 
-        taskMap.put(endTaskName, endPassTask);
+        taskMap.put(DAG_END_TASK_NAME, endPassTask);
         dag.getTasks().add(endPassTask);
         return endPassTask;
     }
 
     /**
      * 处理获取描述符时的逻辑
-     * @param descriptor 描述符字符串
-     * @return 处理后的描述符字符串
      */
     @Override
     public String onRetrieval(String descriptor) {
@@ -310,7 +288,7 @@ public class ClientDAGProcessStrategy implements DAGProcessStrategy {
             return descriptor;
         }
 
-        // 2. 对非结束节点的任务进行处理，包括 inputMappings、outputMappings 等的处理
+        // 2. 对非结束节点的任务进行处理，包括 inputMappings、outputMappings 等的处理，同时在任务列表中删除 DAG 结束节点
         List<BaseTask> tasks = taskMap.values().stream()
                 .filter(task -> !task.getName().equals(dag.getEndTaskName()))
                 .map(task -> processTask(task, dag.getEndTaskName())).toList();
@@ -323,9 +301,7 @@ public class ClientDAGProcessStrategy implements DAGProcessStrategy {
 
     /**
      * 判断是否需要后续处理
-     * @param dag DAG对象
-     * @param taskMap 任务映射
-     * @return 如果DAG有结束任务名称或任何任务有非空输入，则返回true；否则返回false
+     * @return 如果DAG有结束任务或任何一个任务有 input 配置，则返回true，否则返回false
      */
     private boolean needsPostProcessing(DAG dag, Map<String, BaseTask> taskMap) {
         return StringUtils.isNotBlank(dag.getEndTaskName()) ||
@@ -334,9 +310,6 @@ public class ClientDAGProcessStrategy implements DAGProcessStrategy {
 
     /**
      * 处理单个任务，包括 outputMappings、inputMappings、next 的处理
-     * @param task 待处理的任务
-     * @param endTaskName 结束任务的名称
-     * @return 处理后的任务
      */
     private BaseTask processTask(BaseTask task, String endTaskName) {
         // 1. 处理 outputMappings，删除自动生成的配置项
@@ -344,27 +317,24 @@ public class ClientDAGProcessStrategy implements DAGProcessStrategy {
         // 2. 处理 inputMappings，删除存在于 input 中的配置项
         processInputMappingsWhenGetDescriptor(task);
         // 3. 处理 next，删除指向 endTask 的指针
-        processNext(task, endTaskName);
+        removeFromNext(task, endTaskName);
         return task;
     }
 
     /**
-     * 处理任务的 next 属性，删除指向 endTask 的指针
-     * @param task 待处理的任务
-     * @param endTaskName 结束任务的名称
+     * 处理任务的 next 属性，删除指向 taskName 的指针
      */
-    private void processNext(BaseTask task, String endTaskName) {
-        if (task.getNext() == null || StringUtils.isEmpty(endTaskName)) {
+    private void removeFromNext(BaseTask task, String taskName) {
+        if (task.getNext() == null || StringUtils.isEmpty(taskName)) {
             return;
         }
         Set<String> nextSet = new LinkedHashSet<>(Arrays.asList(task.getNext().split(",")));
-        nextSet.remove(endTaskName);
+        nextSet.remove(taskName);
         task.setNext(String.join(",", nextSet));
     }
 
     /**
-     * 处理获取描述符时的 outputMappings
-     * @param task 待处理的任务
+     * 处理获取描述符时的 outputMappings，删除自动生成的配置项
      */
     private void processOutputMappingsWhenGetDescriptor(BaseTask task) {
         List<Mapping> outputMappings = task.getOutputMappings();
@@ -382,7 +352,6 @@ public class ClientDAGProcessStrategy implements DAGProcessStrategy {
 
     /**
      * 处理获取描述符时的 inputMappings, 从任务的 inputMappings 中删除存在于 input 中的配置项
-     * @param task 待处理的任务
      */
     private void processInputMappingsWhenGetDescriptor(BaseTask task) {
         List<Mapping> inputMappings = task.getInputMappings();
