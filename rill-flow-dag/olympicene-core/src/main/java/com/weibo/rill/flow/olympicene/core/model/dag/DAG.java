f722697fae8d7c739798b478b0569f6e4ebfb7e8
zeyu10 <zeyu10@staff.sina.com>
2024-10-29 18:20:24 +0800

    add  @JsonAlias

--- rill-flow-dag/olympicene-core/src/main/java/com/weibo/rill/flow/olympicene/core/model/dag/DAG.java
+++ rill-flow-dag/olympicene-core/src/main/java/com/weibo/rill/flow/olympicene/core/model/dag/DAG.java
@@ -45,7 +45,6 @@ public class DAG {
     @Setter
     private List<BaseResource> resources;
     @JsonProperty("callback")
-    @JsonAlias("callback_config")
     private CallbackConfig callbackConfig;
     @Setter
     private Map<String, String> defaultContext;
@@ -64,7 +63,7 @@ public class DAG {
                @JsonProperty("timer") Timeline timeline,
                @JsonProperty("tasks") List<BaseTask> tasks,
                @JsonProperty("resources") List<BaseResource> resources,
-               @JsonProperty("callback") CallbackConfig callbackConfig,
+               @JsonProperty("callback") @JsonAlias("callback_config") CallbackConfig callbackConfig,
                @JsonProperty("defaultContext") Map<String, String> defaultContext,
                @JsonProperty("commonMapping") Map<String, List<Mapping>> commonMapping,
                @JsonProperty("namespace") String namespace,
